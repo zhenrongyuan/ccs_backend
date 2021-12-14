f4b9db739da209163ffe5efe8a8911b97445d8b0
zhenrongyuan <30374567+zhenrongyuan@users.noreply.github.com>
2021-12-14 00:46:14 -0300

    Cálculo de cuenta.
    Se calcula la cuenta.

--- controllers/CuentaController.js
+++ controllers/CuentaController.js
@@ -2,16 +2,18 @@
 let ModeloObjeto = require('../models/Comanda');
 let EstadoCuenta = require('../models/EstadoCuenta');
 
+async function obtenerEstado(valor) {
+    let estado = await EstadoCuenta.findOne({ clave: valor }).exec();
+    return estado;
+}
+let estado_cuenta = obtenerEstado('SOL');
 let cuenta = {
-    subtotal: Number,
-    descuento: Number,
-    propina: Number,
-    total: Number,
-    comentario: String,
-    estado: {
-        clave: String,
-        glosa: String
-    }
+    subtotal: 0,
+    descuento: 0,
+    propina: 0,
+    total: 0,
+    comentario: '',
+    estado: { estado_cuenta }
 };
 
 async function calcularCuenta(request, response) {
@@ -23,10 +25,10 @@ async function calcularCuenta(request, response) {
         } else {
             if (modeloObtenido) {;
                 let comanda_calculada = calcular(modeloObtenido);
-                let estado = await EstadoCuenta.findOne({ clave: 'SOL' }).exec();
-                estado = { "estado": estado };
-                console.log(estado);
-                comanda_calculada.estado = estado;
+                let estadoSolicitada = await EstadoCuenta.findOne({ clave: 'SOL' }).exec();
+                estadoSolicitada = estadoSolicitada;
+                comanda_calculada.cuenta.estado.clave = estadoSolicitada.clave;
+                comanda_calculada.cuenta.estado.glosa = estadoSolicitada.glosa;
                 return response.status(200).send({
                     comanda_calculada
                 })
@@ -37,36 +39,19 @@ async function calcularCuenta(request, response) {
     });
 }
 
-function obtenerEstadoCuenta(request, response, valor) {
-    let query = { clave: valor };
-    EstadoCuenta.findOne(query).exec((error, modeloObtenido) => {
-        if (error) {
-            console.log(error);
-            //onServerError(response, error);
-        } else {
-            if (modeloObtenido) {
-                return modeloObtenido;
-            } else {
-                //onNotFound(response);
-            }
-        }
-    });
-}
-
 function calcular(comanda) {
-    cuenta.subtotal = 0;
-    cuenta.propina = 0;
-
+    comanda.cuenta.subtotal = 0;
+    comanda.cuenta.propina = 0;
+    comanda.cuenta.descuento = 1000;
     comanda.detalle.forEach(function(det) {
         det.producto.monto_neto = det.producto.precio * det.cantidad;
 
-        cuenta.subtotal += det.producto.monto_neto;
+        comanda.cuenta.subtotal += det.producto.monto_neto;
     });
     //comanda.cuenta.total = 1000000 + subtotal;
-    let cuentaConDescuento = cuenta.subtotal - cuenta.descuento;
-    cuenta.propina = cuentaConDescuento * 0.1;
-    cuenta.total = cuentaConDescuento + comanda.cuenta.propina;
-    comanda.cuenta = cuenta;
+    let cuentaConDescuento = comanda.cuenta.subtotal - comanda.cuenta.descuento;
+    comanda.cuenta.propina = cuentaConDescuento * 0.1;
+    comanda.cuenta.total = cuentaConDescuento + comanda.cuenta.propina;
     return comanda;
 }
 
